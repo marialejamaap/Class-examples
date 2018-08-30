257dcc5c05001061a4506d1da1b21ed049de815a
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-08-30 15:01:19 -0500

    Create function1.cpp

---
+++ function1.cpp
@@ -0,0 +1,34 @@
+//Example 1 sum of two numbers using function
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int addition (int a, int b){
+	int r;
+	r=a+b;
+	return r;
+}
+int main(){
+	int z;
+	z=addition(5,3);
+	cout<<"sum a and b is: " << z;
+}
+
+// Example 2 sum of two numbers using function 
+// La función puede estar en cualquier lugar pero siempre debe estar declarada al comienzo del programa, ejemplo:  
+#include <iostream>
+#include <string>
+
+using namespace std;
+int addition (int a, int b);
+int main(){
+	int z;
+	z=addition(5,3);
+	cout<<"sum a and b is: " << z;
+}
+int addition (int a, int b){
+	int r;
+	r=a+b;
+	return r;
+}
