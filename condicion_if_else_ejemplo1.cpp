cc7eb184afe8378f3b957033b3af603e25386f5b
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-10-13 01:57:58 -0500

    Create condicion_if_else_ejemplo1.cpp

---
+++ condicion_if_else_ejemplo1.cpp
@@ -0,0 +1,20 @@
+// Programa de estructura if y else
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ int x=90;// asigno a la variable x de tipo entero el valor de 100
+
+if (x==100){ /*si la condición se cumple se ejecuta el bloque de instrucciones
+                que contiene*/
+    cout << "x is 100" << endl;}
+else {
+    cout << " x is not 100 " <<endl;/*si no se cumple la condición de x==100
+                                      entonces se ejecuta el bloque de instrucciones
+                                      de else */
+}
+
+return 0;
+
+}
