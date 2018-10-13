763844b99701ddf2beca3cbbd31b70a15a5c77b3
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-10-13 01:52:02 -0500

    Create Condicion_if_ejemplo_1.cpp

---
+++ Condicion_if_ejemplo_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ int x=100;// asigno a la variable x de tipo entero el valor de 100
+
+if (x==100){ /*si la condición se cumple se ejecuta el bloque de instrucciones
+                que contiene*/
+    cout << " x is 100" << endl;} // si no se cumple simplemente lo ignora
+if(x==100) { /*si la condición se cumple se ejecuta el bloque de instrucciones
+                que contiene*/
+    cout <<" x is " << x <<endl; /*x es igual a x que es 100 las dos condiciones
+son lo mismo*/}
+
+return 0;
+
+}
